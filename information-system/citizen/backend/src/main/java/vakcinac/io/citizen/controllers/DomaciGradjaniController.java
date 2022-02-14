2589520cf07ed378aef060ddc820cd2527c1bc2d
Luka Serbedzija <jovluk@hotmail.com>
2022-02-14 10:41:18 +0100

    feat: added domaci gradjanin request.

--- information-system/citizen/backend/src/main/java/vakcinac/io/citizen/controllers/DomaciGradjaniController.java
+++ information-system/citizen/backend/src/main/java/vakcinac/io/citizen/controllers/DomaciGradjaniController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vakcinac.io.citizen.models.dgradj.DomaciGradjanin;
+import vakcinac.io.citizen.models.dgradj.TdomaciGradjanin;
+import vakcinac.io.citizen.request.DomaciGradjaninRequest;
 import vakcinac.io.citizen.service.DomaciGradjaninService;
 
 @Controller
@@ -18,8 +20,28 @@ public class DomaciGradjaniController {
 	private DomaciGradjaninService domaciGradjaninService;
 	
 	@PostMapping
-	public ResponseEntity<DomaciGradjanin> createDomaciGradjanin(@RequestBody DomaciGradjanin domaciGradjanin) {
-		DomaciGradjanin createdDomaciGradjanin = domaciGradjaninService.create(domaciGradjanin.getGradjanin().getKorisnickoIme(), domaciGradjanin);
+	public ResponseEntity<DomaciGradjanin> createDomaciGradjanin(@RequestBody DomaciGradjaninRequest domaciGradjaninRequest) {
+		DomaciGradjanin domaciGradjanin = new DomaciGradjanin();
+		TdomaciGradjanin model = new TdomaciGradjanin();
+		domaciGradjanin.setGradjanin(model);
+		
+		model.setIme(domaciGradjaninRequest.getIme());
+		model.setPrezime(domaciGradjaninRequest.getPrezime());
+		model.setKorisnickoIme(domaciGradjaninRequest.getKorisnickoIme());
+		model.setLozinka(domaciGradjaninRequest.getLozinka());
+		model.setDatumRodjenja(domaciGradjaninRequest.getDatumRodjenja());
+		model.setPol(domaciGradjaninRequest.getPol());
+		model.setEmail(domaciGradjaninRequest.getEmail());
+		model.setImeRoditelja(domaciGradjaninRequest.getImeRoditelja());
+		model.setMestoRodjenja(domaciGradjaninRequest.getMestoRodjenja());
+		model.setAdresa(domaciGradjaninRequest.getAdresa());
+		model.setMesto(domaciGradjaninRequest.getMesto());
+		model.setOpstina(domaciGradjaninRequest.getOpstina());
+		model.setJmbg(domaciGradjaninRequest.getJmbg());
+		model.setBrojMobilnogTelefona(domaciGradjaninRequest.getBrojMobilnogTelefona());
+		model.setBrojFiksnogTelefona(domaciGradjaninRequest.getBrojFiksnogTelefona());
+		
+		DomaciGradjanin createdDomaciGradjanin = domaciGradjaninService.create(model.getKorisnickoIme(), domaciGradjanin);
 		
 		return ResponseEntity.ok(createdDomaciGradjanin);
 	}
