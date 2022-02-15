7a9ef6b30a32e3fbb043c00e312a0516ce579d6a
Luka Bjelica <bjelicaluka99@gmail.com>
2022-02-15 16:46:52 +0100

    refactor: sidebar

--- vakcinac.io-frontend/src/components/Sidebar.vue
+++ vakcinac.io-frontend/src/components/Sidebar.vue
@@ -1,59 +1,55 @@
 <template>
-  <v-card
-    class="mx-auto overflow-hidden"
-    height="100vh"
+<div>
+  <v-navigation-drawer
+    v-model="drawer"
+    absolute
+    temporary
   >
-    <v-app-bar>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6">
+          {{title}}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{subtitle}}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
 
-      <v-spacer></v-spacer>
+    <v-divider></v-divider>
 
-      <v-toolbar-title>{{userFullName}}</v-toolbar-title>
-
-      <v-btn icon>
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      bottom
-      temporary
+    <v-list
+      dense
+      nav
     >
-      <v-list-item>
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        link
+        :to="item.href"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
         <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            {{title}}
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            {{subtitle}}
-          </v-list-item-subtitle>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-app-bar>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-divider></v-divider>
+    <v-spacer></v-spacer>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+    <v-toolbar-title>{{userFullName}}</v-toolbar-title>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+    <v-btn icon>
+      <v-icon>mdi-logout</v-icon>
+    </v-btn>
+  </v-app-bar>
+</div>
 </template>
 
 <script>
