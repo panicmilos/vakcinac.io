f0e33f86fb9e03324fe970efa734bfef0fc5e24f
Luka Bjelica <bjelicaluka99@gmail.com>
2022-02-15 16:56:30 +0100

    feat: added generic table component

---
+++ vakcinac.io-frontend/src/components/Table/TableCells/StyledTextCell.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+  <i v-if="!!props.icon" :class="`feather icon-${props.icon} f-16`"></i>
+  <span :class="`text-${props.color || 'secondary'} text-xs font-weight-bold`">{{props.label}}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    props: {}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vakcinac.io-frontend/src/components/Table/TableRow.vue
@@ -0,0 +1,54 @@
+<template>
+  <tr>
+    <td class="px-4" v-for="(value, index) in values" :key="index">
+      <component :is="tableCellComponent(value)" :props="value" />
+    </td>
+    <td v-if="renderActions" class="other-left">
+      <slot />
+    </td>
+  </tr>
+</template>
+
+<script>
+import StyledTextCellVue from './TableCells/StyledTextCell.vue';
+import TextCellVue from './TableCells/TextCell.vue'
+
+const cellTypeComponentRegistry = {
+  'styledText': StyledTextCellVue
+};
+
+export default {
+  components: {
+
+  },
+  props: {
+    values: {
+      type: Array
+    },
+    renderActions: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    tableCellComponent(value) {
+      if(typeof value === 'string' || value instanceof String) {
+        return TextCellVue;
+      } else {
+        return cellTypeComponentRegistry[value.type];
+      }
+    }
+  }
+}
+</script>
+
+<style>
+td {
+  white-space: normal !important;
+  overflow-wrap: break-word;
+  /* word-wrap: break-word; */
+}
+.other-left {
+  text-align: right
+}
+</style>
