c2c0e9f5b6a3e800b287c23991f5ac3fd9915e93
Panic <panic.milos99@gmail.com>
2022-02-17 17:15:22 +0100

    fix: fixed imports.

--- information-system/citizen/backend/src/main/java/vakcinac/io/citizen/service/SaglasnostService.java
+++ information-system/citizen/backend/src/main/java/vakcinac/io/citizen/service/SaglasnostService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
