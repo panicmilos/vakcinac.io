95a4e01e1a6fc9906f5841452e0785ec27466dd3
begnoo <zoranjankov99@gmail.com>
2022-02-18 03:18:27 +0100

    feat: changed rdf search to return createdAt with document

--- information-system/citizen/backend/src/main/java/vakcinac/io/citizen/controllers/RdfSearchController.java
+++ information-system/citizen/backend/src/main/java/vakcinac/io/citizen/controllers/RdfSearchController.java
@@ -1,4 +1,4 @@
-package vakcinac.io.civil.servant.controllers;
+package vakcinac.io.citizen.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import vakcinac.io.civil.servant.service.RdfSearchService;
+import vakcinac.io.citizen.service.RdfSearchService;
 import vakcinac.io.core.requests.MetaSearchRequest;
 import vakcinac.io.core.results.doc.QueryDocumentsResult;
 
